3c5e72b735f739f551292fb3277cd9dbf922861a
Shope Alli-B <zoeyalli921@gmail.com>
2025-05-31 17:37:09 -0700

    new commit

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://expense-tracker-4q64.onrender.com/api', 
+  baseURL: 'https://expense-tracker-4q64.onrender.com', 
   headers: {
     'Content-Type': 'application/json'
   }
